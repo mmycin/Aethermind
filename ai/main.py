dd4049020cb7d7ff76be3427fdbef4dccb12d648
Tahcin Ul Karim Mycin <150533238+mmycin@users.noreply.github.com>
2025-11-07 22:11:09 +0600

    Analytics Data

--- ai/main.py
+++ ai/main.py
@@ -1,5 +1,17 @@
+from schema.schema_pb2 import UserAttention, LowAttentionReason
+
 def main():
-    print("Hello from eda!")
+    user1 = UserAttention(
+    Name="Alice",
+       Age=20,
+       Gender="Female",
+       Residence="Dhaka",
+       Reason_las=LowAttentionReason.MULTITASKING,
+       Screentime=5.5,
+       Attention_Span=2.3
+    )
+    
+    print(type(user1))
 
 
 if __name__ == "__main__":
